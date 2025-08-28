14081e47c1c56667f4900a83648422b0e50877a9
David Caseria <davidcaseria@gmail.com>
2025-08-28 11:06:02 -0400

    Use release-smaller profile

--- build-xcframework.sh
+++ build-xcframework.sh
@@ -23,20 +23,20 @@ rustup target add aarch64-apple-darwin   # mac M1
 rustup target add x86_64-apple-darwin    # mac x86_64
 
 # Build cdk-ffi rust lib for apple targets
-cargo build --package cdk-ffi --release --target x86_64-apple-darwin
-cargo build --package cdk-ffi --release --target aarch64-apple-darwin
-cargo build --package cdk-ffi --release --target x86_64-apple-ios
-cargo build --package cdk-ffi --release --target aarch64-apple-ios
-cargo build --package cdk-ffi --release --target aarch64-apple-ios-sim
+cargo build --package cdk-ffi --profile release-smaller --target x86_64-apple-darwin
+cargo build --package cdk-ffi --profile release-smaller --target aarch64-apple-darwin
+cargo build --package cdk-ffi --profile release-smaller --target x86_64-apple-ios
+cargo build --package cdk-ffi --profile release-smaller --target aarch64-apple-ios
+cargo build --package cdk-ffi --profile release-smaller --target aarch64-apple-ios-sim
 
 # Build cdk-ffi Swift bindings and put in cdk-swift Sources
-cargo run --bin uniffi-bindgen generate --library ./target/aarch64-apple-ios/release/libcdk_ffi.dylib --language swift --out-dir ../cdk-swift/Sources/CashuDevKit --no-format
+cargo run --bin uniffi-bindgen generate --library ./target/aarch64-apple-ios/release-smaller/libcdk_ffi.dylib --language swift --out-dir ../cdk-swift/Sources/CashuDevKit --no-format
 
 # Combine cdk-ffi static libs for universal binaries via lipo tool
-mkdir -p target/lipo-ios-sim/release
-lipo target/aarch64-apple-ios-sim/release/libcdk_ffi.a target/x86_64-apple-ios/release/libcdk_ffi.a -create -output target/lipo-ios-sim/release/libcdk_ffi.a
-mkdir -p target/lipo-macos/release
-lipo target/aarch64-apple-darwin/release/libcdk_ffi.a target/x86_64-apple-darwin/release/libcdk_ffi.a -create -output target/lipo-macos/release/libcdk_ffi.a
+mkdir -p target/lipo-ios-sim/release-smaller
+lipo target/aarch64-apple-ios-sim/release-smaller/libcdk_ffi.a target/x86_64-apple-ios/release-smaller/libcdk_ffi.a -create -output target/lipo-ios-sim/release-smaller/libcdk_ffi.a
+mkdir -p target/lipo-macos/release-smaller
+lipo target/aarch64-apple-darwin/release-smaller/libcdk_ffi.a target/x86_64-apple-darwin/release-smaller/libcdk_ffi.a -create -output target/lipo-macos/release-smaller/libcdk_ffi.a
 
 cd ../cdk-swift/ || exit
 
@@ -51,10 +51,10 @@ rm -rf "${OUTDIR}/${NAME}.xcframework"
 
 # Create new xcframework from cdk-ffi static libs and headers
 xcodebuild -create-xcframework \
-    -library "${TARGETDIR}/lipo-macos/release/${STATIC_LIB_NAME}" \
+    -library "${TARGETDIR}/lipo-macos/release-smaller/${STATIC_LIB_NAME}" \
     -headers "${NEW_HEADER_DIR}" \
-    -library "${TARGETDIR}/aarch64-apple-ios/release/${STATIC_LIB_NAME}" \
+    -library "${TARGETDIR}/aarch64-apple-ios/release-smaller/${STATIC_LIB_NAME}" \
     -headers "${NEW_HEADER_DIR}" \
-    -library "${TARGETDIR}/lipo-ios-sim/release/${STATIC_LIB_NAME}" \
+    -library "${TARGETDIR}/lipo-ios-sim/release-smaller/${STATIC_LIB_NAME}" \
     -headers "${NEW_HEADER_DIR}" \
     -output "${OUTDIR}/${NAME}.xcframework"
